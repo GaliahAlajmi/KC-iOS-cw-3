429b3a22db64713904ddcb83f98f6bf17fe4bd6e
GaliahAlajmi <gahlah2005@gmail.com>
2022-08-19 14:51:40 +0300

    app3

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var favouritenumber:Int=2
+var name:String="Galiah"
+var hobby="reading &designing"
+
+print("hello my name is \(name),My favourite number is\(favouritenumber), My hobby is \(hoppy)and I was born at\(birthofdate) ")
+
+let birthofdate=2005
+
